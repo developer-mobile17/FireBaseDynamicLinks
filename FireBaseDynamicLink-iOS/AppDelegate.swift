56389eb8e49ad9969955036e2ba23e17f5bb8a25
Vikas Singh <Vikas.mca11@gmail.com>
2023-07-28 21:11:27 +0530

    Firebase Dynamic link implementation into iOS.

--- FireBaseDynamicLink-iOS/AppDelegate.swift
+++ FireBaseDynamicLink-iOS/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import firebbaseDynamiclinks
+// Import FirBaseDynamiclinks
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,23 +16,73 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    // MARK: Firebase Dynamic
+    
+    @available(iOS 9.0, *)
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
+      return application(app, open: url,
+                         sourceApplication: options[UIApplication.OpenURLOptionsKey
+                           .sourceApplication] as? String,
+                         annotation: "")
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    
+    // MARK: Use this if you have custom domain link like.  www.abc.com
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?,
+                     annotation: Any) -> Bool {
+      if let dynamicLink = DynamicLinks.dynamicLinks().dynamicLink(fromCustomSchemeURL: url) {
+        // Handle the deep link. For example, show the deep-linked content or
+        // apply a promotional offer to the user's account.
+        // ...
+        return true
+      }
+      return false
     }
-
-
+    
+    // MARK: This will call when you will click any Firebase dynamic Link outside the application like safari or mail or message.
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+            print("Handeling dynamic link")
+            if let incomingURL = userActivity.webpageURL {
+                print("Incoming URL is \(incomingURL)")
+                let linkHandled = DynamicLinks.dynamicLinks().handleUniversalLink(incomingURL) { (dynamicLink, error) in
+                    guard error == nil else {
+                        print("Found an error! \(error!.localizedDescription)")
+                        return
+                    }
+                    if let dynamicLink = dynamicLink {
+                        self.handleIncomingDynamicLink(dynamicLink)
+                    }
+                }
+                if linkHandled {
+                    return true
+                } else {
+                    // Maybe do other things with our incoming url
+                    return false
+                }
+            }
+            return false
+        }
+    
+    
+    
+    func handleIncomingDynamicLink(_ dynamicLink: DynamicLink) {
+            guard let url = dynamicLink.url else {
+                print("That is weird. My dynamic link object has no url")
+                return
+            }
+            print("Your incoming link parameter is \(url.absoluteString)")
+    }
+    
 }
 
---
+++ FireBaseDynamicLink-iOS/ReadMeSetupinstruction.swift
@@ -0,0 +1,52 @@
+//
+//  ReadMeSetupinstruction.swift
+//  FireBaseDynamicLink-iOS
+//
+//  Created by vikas on 28/07/23.
+//
+
+import Foundation
+
+
+/*
+ 
+ Please follow below setup instruction
+ 
+ 1: install pod file for dynamic links
+ refer: https://cocoapods.org/pods/FirebaseDynamicLinks#standard-pod-install
+ 
+ 2: Configure Firebase into appdelegate at didfinishlaunching method.
+ 
+ 3: configure your App ID and Bundle Identifier on firebase console for dynamic links.
+ 
+ 4: In your Xcode project's Info.plist file, create a key called FirebaseDynamicLinksCustomDomains and set it to your app's Dynamic Links URL prefixes. For example:
+ 
+ <key>FirebaseDynamicLinksCustomDomains</key>
+ <array>
+   <string>https://example.com/link</string>
+   <string>https://example.com/promos</string>
+ </array>
+ 
+ 
+ 
+ 
+ // MARK: Receive Dynamic Links
+ Now we will configure and write some code that how code will react when it receives Dynamic link. Let’s get started
+
+ Configuration
+ First, we will enable Associated Domain capability in identifiers. For that Follow these steps:
+
+ Login in Apple Developer Account.
+ Goto Certificates, Identifiers & Profiles.
+ Select Identifiers.
+ Open the identifier of your application.
+ Mark the Associated Domains.
+ Then save it.
+ We are done here now we will configure our dynamic link in our project.
+
+ Open Project and select target then go to Signing & capabilities.
+ Add capability Associated Domains.
+ Now add Domain like this applinks:appnamee.page.link
+
+ 
+ */
